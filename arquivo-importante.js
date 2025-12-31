b974213659f1751ae3aa3bba6af76b688fd7e9b0
raulfranck <raulengfranck@gmail.com>
2025-12-31 15:45:45 -0300

    removendo comentarios

--- arquivo-importante.js
+++ arquivo-importante.js
@@ -1,7 +1,8 @@
 console.log("Esse codigo e importante para o projeto");
+console.log("Esse codigo e importante para o projeto");
 
 function importantFunction(a, b) {
-    return null
+    return undefined
 }
 
 console.log(somar(1, 2));
--- test.js
+++ test.js
@@ -1,4 +1,7 @@
 console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World"); 
 
 /**
  * Busca Binária em um array ordenado
@@ -18,20 +21,20 @@ function buscaBinaria(array, alvo) {
     }
 
     if (array[meio] < alvo) {
-      esquerda = meio + 1; // Busca na metade direita
+      esquerda = meio + 1; 
     } else {
-      direita = meio - 1; // Busca na metade esquerda
+      direita = meio - 1; 
     }
   }
 
-  return -1; // Elemento não encontrado
+  return -1; 
 }
 
 // Exemplo de uso com array de 100 posições
-const array100 = Array.from({ length: 100 }, (_, i) => i + 1); // [1, 2, 3, ..., 100]
+const array100 = Array.from({ length: 100 }, (_, i) => i + 1); 
 
 // Testando a busca
-console.log("Buscando 42:", buscaBinaria(array100, 42));   // Retorna 41 (índice)
+console.log("Buscando 42:", buscaBinaria(array100, 42));   
 console.log("Buscando 1:", buscaBinaria(array100, 1));     // Retorna 0 (índice)
 console.log("Buscando 100:", buscaBinaria(array100, 100)); // Retorna 99 (índice)
 console.log("Buscando 150:", buscaBinaria(array100, 150)); // Retorna -1 (não existe)
